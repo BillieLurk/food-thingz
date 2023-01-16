4a90ae6aaa50964b4d03c930d649e988e7f575c3
BillieLurk <bill@1618holding.com>
2023-01-16 09:31:13 +0100

    final build

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
 import styled from "styled-components";
 
 import App from "../components/App";
@@ -31,7 +28,7 @@ export async function getServerSideProps() {
 
 
 
-export default function Home({foodPlace} : {foodPlace:any}) {
+export default function Home({foodPlace}) {
   return (
     <>
       <Head>
